a30e7421f605d310a977b098c73c050c9258659c
Abeinevincent <abeinevincent@gmail.com>
2023-02-19 14:31:47 +0300

    Added setup for unit testing using jest

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,5 @@ export default {
     "\\.(gif | tif | eot | svg | png)$": "<rootDir>/test/__mocks__/fileMock.js",
     "  \\.(css|less|sass|scss)$": "identity-obj-proxy",
   },
+  setupFilesAfterEnv: "[<rootDir>/jest.setup.js]",
 };
