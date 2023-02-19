c6c491f7d3d7aa99d9d475e170cadd2c0af70aad
Abeinevincent <abeinevincent@gmail.com>
2023-02-19 17:36:09 +0300

    Added setup for unit testing using jest

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 function App() {
   return (
     <div className="App">
+      <p>Good news, all set, project can now commence!</p>
       <button>Sample Button clicked: ButtonCountHere</button>
     </div>
   );
