f74e14dde078f24194d814103ae601508d032caa
Abeinevincent <abeinevincent@gmail.com>
2023-02-19 17:37:14 +0300

    Unit testing setup complete

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { useState } from "react";
 function App() {
   return (
     <div className="App">
-      <p>Good news, all set, project can now commence since all is good!</p>
+      <p>
+        Good news, all set, project can now commence since all is good good!
+      </p>
       <button>Sample Button clicked: ButtonCountHere</button>
     </div>
   );
