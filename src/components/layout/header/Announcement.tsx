2ba88f11dc2b2e8c175a037d19c33cd4e9ac1d38
Abeinevincent <abeinevincent@gmail.com>
2023-02-28 09:28:36 +0300

    Added Announcent Component

--- src/components/layout/header/Announcement.tsx
+++ src/components/layout/header/Announcement.tsx
@@ -1,14 +1,37 @@
 import { Flex, Box, Text, Icon } from "@chakra-ui/react";
 import { TertiaryColor } from "../../../theme/GlobalStyles";
 import { BsEnvelope } from "react-icons/bs";
+import { FaDonate } from "react-icons/fa";
 
 const Announcement = () => {
   return (
     <Flex bg={TertiaryColor} h={12} justifyContent="space-around">
-      <Box display="flex" gap={2} alignItems="center" justifyContent={"center"}>
-        <Icon color="white" as={BsEnvelope} />
-
-        <Text textColor="white">Donate</Text>
+      <Box
+        display="flex"
+        gap={2}
+        alignItems="center"
+        flexWrap={"wrap-reverse"}
+        justifyContent={"space-between"}
+        // style={{ justifyContent: "space-between!important" }}
+      >
+        <Box
+          display="flex"
+          gap={2}
+          alignItems="center"
+          justifyContent={"space-around"}
+        >
+          <Icon color="white" as={BsEnvelope} />
+          <Text color={"white"}>codewithvicent@gmail.com</Text>
+        </Box>
+        <Box
+          display="flex"
+          gap={2}
+          alignItems="center"
+          justifyContent={"space-around"}
+        >
+          <Icon color="white" as={FaDonate} />
+          <Text textColor="white">Donate</Text>
+        </Box>
       </Box>
     </Flex>
   );
