4001fafc5de4776c70e3a354b5e7d22627c75f69
Abeinevincent <abeinevincent@gmail.com>
2023-03-31 18:03:33 +0300

    Testing Redirect Rule

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,7 +7,7 @@ export const Home = () => {
       <Hero />
       <Box p={{base: 5, md: 10, lg: 20}}>
         <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+          Testing content dolor sit amet consectetur adipisicing elit. Excepturi
           deleniti id neque veritatis et maiores, voluptas laborum, placeat sint
           mollitia accusantium commodi, accusamus non ducimus saepe dolore vitae
           quasi fugit.
